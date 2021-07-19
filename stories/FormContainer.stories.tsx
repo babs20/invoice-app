f16ddab1c784ae6d5e4d62657c34f8c6bfb6bd83
Brady Blair <thevillemc@gmail.com>
2021-07-19 14:18:06 -0400

    add basic form

---
+++ stories/FormContainer.stories.tsx
@@ -0,0 +1,22 @@
+import React, { ComponentProps } from 'react';
+
+import { Story, Meta } from '@storybook/react';
+
+import FormContainer from '../layouts/FormContainer';
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  title: 'FormContainer',
+  component: FormContainer,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta;
+
+//👇 We create a “template” of how args map to rendering
+const Template: Story<ComponentProps<typeof FormContainer>> = (args) => (
+  <FormContainer />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
